223e49408d2ef8937b44290d29148d83c1ed863b
Hasham Asad <tipuasad2001@gmail.com>
2024-06-07 07:17:42 +0500

    hostingkrnihaiab

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import FileList from './app/FilesList';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { FaLinkedin, FaGithub, FaEnvelope, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope, FaInstagram } from 'react-icons/fa';
 
 function App() {
   return (
@@ -10,6 +10,9 @@ function App() {
       <Navbar bg="dark" variant="dark" className="mb-3">
         <Container>
           <Navbar.Brand href="#home">Hasham Asad Projects Files</Navbar.Brand>
+          <div className="header-info ml-auto">
+            <span style={{fontStyle: 'italic'}} className="text-white font-weight-light font-italic">Click any card to download the file</span>
+          </div>
           <Nav className="ml-auto">
             <Nav.Link href="https://www.linkedin.com/in/hasham-asad-44ab9926a/" target="_blank">
               <FaLinkedin className="text-light" />
@@ -19,9 +22,6 @@ function App() {
             </Nav.Link>
             <Nav.Link href="mailto:tipuasad2001@gmail.com" target="_blank">
               <FaEnvelope className="text-light" />
-            </Nav.Link>
-            <Nav.Link href="https://twitter.com/yourusername" target="_blank">
-              <FaTwitter className="text-light" />
             </Nav.Link>
             <Nav.Link href="https://www.instagram.com/?next=%2F" target="_blank">
               <FaInstagram className="text-light" />
--- src/app/FilesList.jsx
+++ src/app/FilesList.jsx
@@ -4,9 +4,11 @@ import { Card, Col, Row } from 'react-bootstrap';
 
 
 const files = [
-  { name: 'Project1.exe', size: '15 MB', type: 'Executable', details: 'Windows executable for Project 1', url: '/path/to/Project1.exe', logo: './logo192.png' },
-  { name: 'Project2.apk', size: '25 MB', type: 'Android Package', details: 'Android APK for Project 2', url: '/path/to/Project2.apk', logo: '/path/to/logo2.png' },
-  { name: 'Project3.love', size: '10 MB', type: 'Game File', details: 'LOVE2D game file for Project 3', url: '/path/to/Project3.love', logo: '/path/to/logo3.png' },
+  { name: 'Chatting', size: '100.97 MB', type: '.exe', details: 'A Next.js Application Converted to Desktop Application using Electron', url: '/files/chatting-electron.exe', logo: 'https://cdn.worldvectorlogo.com/logos/electron-4.svg' },
+  { name: 'ChildrenCare', size: '723 MB', type: '.apk', details: 'Android created in React Native, A guid on tracheostomy care in children', url: '/files/childrenCare.apk', logo: '/logos/trachealLogo.jpg' },
+  { name: 'Pong', size: '71 MB', type: '.love', details: 'My touch on Pong a game 1972, created in lua and love2d the game consist of both single player and multiplayer game play', url: '/files/pong.love', logo: '/logos/pongLogo.png' },
+  {name: 'MovieBrowser', size: 'NA', type: '.apk', details: 'Work Under Progress', url: '', logo: '/logos/movieIcon.jpg'},
+  {name: 'Flappy', size: 'NA', type: '.love', details: 'My take a on flappy bird, the game awards you with madels on different milestones and contains random geration of levels, created in love2d', url: '', logo: 'https://www.wired.com/images_blogs/gamelife/2014/02/title.jpg'}
   // Add more files as needed
 ];
 
@@ -15,10 +17,11 @@ const FileList = () => {
     <Row>
       {files.map((file, index) => (
         <Col md={4} key={index} className="mb-4">
-          <a href={file.url} download={file.name} className="file-download-link">
-            <Card className="file-card" style={{ backgroundImage: `url(${file.logo})` }}>
+          <a href={file.url} type={file.type} download={`${file.name}${file.type}`} className="file-download-link">
+            <Card className="file-card" style={file.name === 'Pong' || file.name.includes('M') ? {color:'white', backgroundImage: `url(${file.logo})`}:{ backgroundImage: `url(${file.logo})`, 
+            backgroundSize: 'cover'}}>
               <Card.Body>
-                <Card.Title>{file.name}</Card.Title>
+                <Card.Title >{file.name}</Card.Title>
                 <Card.Text>Size: {file.size}</Card.Text>
                 <Card.Text>Type: {file.type}</Card.Text>
                 <div className="file-details">
