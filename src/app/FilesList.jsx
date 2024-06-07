66dabb374cadf6bd1f6ce4f053d3a4783125175d
Hasham Asad <tipuasad2001@gmail.com>
2024-06-08 04:51:52 +0500

    filesInsideSrc

--- src/app/FilesList.jsx
+++ src/app/FilesList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 
+// Logos
+import pongLogo from './logos/pongLogo.png'
+import childCareLogo from './logos/trachealLogo.jpg'
+
+// Files
+import chatting from './files/chatting-electron.exe'
+import pong from './files/pong.love'
+import childCare from './files/childrenCare.apk'
+
 
 const files = [
-  { name: 'Chatting', size: '100.97 MB', type: '.exe', details: 'A Next.js Application Converted to Desktop Application using Electron', url: '/files/chatting-electron.exe', logo: 'https://cdn.worldvectorlogo.com/logos/electron-4.svg' },
-  { name: 'ChildrenCare', size: '723 MB', type: '.apk', details: 'Android App created in React Native, A guide on tracheostomy care in children', url: '/files/childrenCare.apk', logo: '/logos/trachealLogo.jpg' },
-  { name: 'Pong', size: '71 MB', type: '.love', details: 'My touch on Pong a game 1972, created in lua and love2d the game consist of both single player and multiplayer game play', url: '/files/pong.love', logo: '/logos/pongLogo.png' },
+  { name: 'Chatting', size: '100.97 MB', type: '.exe', details: 'A Next.js Application Converted to Desktop Application using Electron', url: chatting, logo: 'https://cdn.worldvectorlogo.com/logos/electron-4.svg' },
+  { name: 'ChildrenCare', size: '723 MB', type: '.apk', details: 'Android App created in React Native, A guide on tracheostomy care in children', url: childCare, logo: childCareLogo },
+  { name: 'Pong', size: '71 MB', type: '.love', details: 'My touch on Pong a game 1972, created in lua and love2d the game consist of both single player and multiplayer game play', url: pong, logo: pongLogo },
   // {name: 'MovieBrowser', size: 'NA', type: '.apk', details: 'Work Under Progress', url: '', logo: 'https://t4.ftcdn.net/jpg/05/00/61/19/360_F_500611919_5wuf1qGRCubiXXxIa7og1fLLCyHi6qP9.jpg'},
   // {name: 'Flappy', size: 'NA', type: '.love', details: 'My take a on flappy bird, the game awards you with madels on different milestones and contains random geration of levels, created in love2d', url: '', logo: 'https://www.wired.com/images_blogs/gamelife/2014/02/title.jpg'}
   // Add more files as needed
