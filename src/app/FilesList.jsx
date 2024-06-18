0c592148b8fe0acc8141521418bf06da874b9a3b
Hasham Asad <tipuasad2001@gmail.com>
2024-06-18 14:57:02 +0500

    'breakout'

--- src/app/FilesList.jsx
+++ src/app/FilesList.jsx
@@ -11,23 +11,10 @@ const files = [
   { name: 'MovieBrowser', size: 'NA', type: '.apk', details: 'Work Under Progress', url: '', logo: 'https://t4.ftcdn.net/jpg/05/00/61/19/360_F_500611919_5wuf1qGRCubiXXxIa7og1fLLCyHi6qP9.jpg', style: {} },
   { name: 'Flappy', size: '12 MB', type: '.love', details: 'My take a on flappy bird, the game awards you with madels on different milestones and contains random geration of levels, created in love2d', url: 'files/flappy.love', logo: 'https://www.wired.com/images_blogs/gamelife/2014/02/title.jpg', style: {} },
   { name: 'AttendanceSystem', size: '50 MB', type: '.pt', details: 'yoloV8 trained on custom dataset for the purpose of face recognization Attendace system', url: 'files/yolov8m_custom.pt', logo: 'https://assets-global.website-files.com/646dd1f1a3703e451ba81ecc/664f72537d932bad2865e9d0_Ultralytics_Website.jpg', style: {} },
-  { name: 'tic-tac-toe', size: '30 MB', type: '.zip', details: 'Tic-Tac-Toe, Ai as your competition, uses min max algorithum to compute the next best box either for blocking or achieving victory (extract from zip file then run the runner.exe)', url: 'files/tic-tac-toe.zip', logo: 'https://play-lh.googleusercontent.com/sZS1Ea6_NN2-yyNyhOdc-u_TPq4d49q3qMsNfo1OsemxtID18-2y9OtW9pt_SevZF-wN', style: { backgroundSize: 'cover' } }
+  { name: 'tic-tac-toe', size: '30 MB', type: '.zip', details: 'Tic-Tac-Toe, Ai as your competition, uses min max algorithum to compute the next best box either for blocking or achieving victory (extract from zip file then run the runner.exe)', url: 'files/tic-tac-toe.zip', logo: 'https://play-lh.googleusercontent.com/sZS1Ea6_NN2-yyNyhOdc-u_TPq4d49q3qMsNfo1OsemxtID18-2y9OtW9pt_SevZF-wN', style: { backgroundSize: 'cover' } },
+  {name: 'Breakout', size: '6.1 MB', type: '.love', details: 'Breakout Recreated in lua Love2d To get The understanding of Quads, game includes 4 power ups a lock block and highscore state', url: 'files/breakout.love', logo: 'logos/breakout.png', style: {color: 'white'}}
   // Add more files as needed
 ];
-
-
-// async function handleDownload(filePath){
-//   try{
-//     const response = await fetch(filePath)
-//     if(response.ok){
-//       const fileInfo = await response.json()
-//       const fileContent = atob(fileInfo.content)
-//       saveAs(fileContent)
-//     }
-//   }catch(err){
-//     console.log(err)
-//   }
-// }
 
 const FileList = () => {
   return (
@@ -38,7 +25,7 @@ const FileList = () => {
             <Card className="file-card"
               style={file.name === 'Pong' || file.name.includes('tic') ?
                 { color: 'white', backgroundImage: `url(${file.logo})`, ...file.style} :
-                { backgroundImage: `url(${file.logo})`, backgroundSize: 'cover' }}>
+                { backgroundImage: `url(${file.logo})`, backgroundSize: 'cover', ...file.style }}>
               <Card.Body>
                 <Card.Title >{file.name}</Card.Title>
                 <Card.Text>Size: {file.size}</Card.Text>
